e58df98028cc2a0e2cfbfb9f3993a3690e146d18
Mercenary <Mercenary@noname.com>
2018-11-06 19:51:32 +0300

    test

---
+++ Core/IFinance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    interface IFinance
+    {
+        string Salary { get; set; }
+
+
+    }
+
+    interface ILogicFinance
+    {
+        void AddSalary();
+        void CalcSalary();
+    }
+}
---
+++ Core/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace StaffLibrary
+{
+   public abstract class Person
+    {
+        /*
+           Фамилия, имя, отчество;
+           Дата, место рождения;
+           Сведения о гражданстве;
+           Паспортные данные;
+           Адрес регистрации;
+           Адрес фактического места проживания;
+         */
+
+    }
+
+    public class Students : Person
+    {
+        /*
+           Сведения о предыдущем образовании;
+           Сведения о воинской обязанности;
+           Сведения о социальных льготах;
+           Сведения о форме обучения;
+           Сведения об образовательной программе обучения;
+           Сведения об основе обучения (договор);
+           Сведения о движении студента (выписки из приказов);
+           Сведения об успеваемости студента.
+         */
+    }
+
+    public class Teacher : Person, IFinance
+    {
+        public string Salary { get; set; }
+        /*
+            Кафедра
+            Ученая степень
+            Должность
+         */
+    }
+
+    public class Staff : Person, IFinance
+    {
+        public string Salary { get; set; }
+        /*
+            Должность
+        */
+    }
+}
